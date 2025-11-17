32fdd1284f4bd12a16a993ee21d941b721203481
Jerico-JH22 <jerico.gatpandan@unc.edu.ph>
2025-11-17 18:19:43 +0800

    Add Label

--- src/main/java/com/cht/TravelAndToursManagement/controller/LoginController.java
+++ src/main/java/com/cht/TravelAndToursManagement/controller/LoginController.java
@@ -1,5 +1,23 @@
 package com.cht.TravelAndToursManagement.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class LoginController {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private void initialize() {
+        button.setText("Click me");
+    }
+
 
 }
